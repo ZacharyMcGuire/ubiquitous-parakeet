8576ff2e22a23ef9e2f6a06b02d3241d6852e4f1
Zachary McGuire <zachary_mcguire@outlook.com>
2020-01-16 21:31:01 +1100

    initial model design

--- forum/models.py
+++ forum/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+class Board(models.Model):
+    board_title = models.CharField(max_length=64, null=False, blank=False)
+    board_description = models.CharField(max_length=128, null=False, blank=False)
+    board_created = models.DateTimeField(verbose_name='date and time the board was created', auto_now_add=True)
+    board_modified = models.DateTimeField(verbose_name='date and time the board was most recently modified', auto_now=True)
+
+class Topic(models.Model):
+    board = models.ForeignKey(to=Board, on_delete=models.CASCADE)
+    topic_title = models.CharField(max_length=64, null=False, blank=False)
+    topic_text = models.TextField(max_length=512, null=False, blank=False)
+    topic_created = models.DateTimeField(verbose_name='date and time the Topic was created', auto_now_add=True)
+    topic_modified = models.DateTimeField(verbose_name='date and time the Topic was most recently modified', auto_now=True)
+
+class Comment(models.Model):
+    topic = models.ForeignKey(to=Topic, on_delete=models.CASCADE)
+    comment_text = models.TextField(max_length=512, null=False, blank=False)
+    comment_created = models.DateTimeField(verbose_name='date and time the Comment was created', auto_now_add=True)
+    comment_modified = models.DateTimeField(verbose_name='date and time the Comment was most recently modified', auto_now=True)
+
