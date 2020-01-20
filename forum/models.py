bbbd7be9e2e48f135c6c6ac3c0c1a39cd9dd90ef
Zachary McGuire <zachary_mcguire@outlook.com>
2020-01-21 07:54:06 +1100

    defined function to return all topics in a board.

--- forum/models.py
+++ forum/models.py
@@ -6,15 +6,19 @@ class Board(models.Model):
     board_created = models.DateTimeField(verbose_name='date and time the board was created', auto_now_add=True)
     board_modified = models.DateTimeField(verbose_name='date and time the board was most recently modified', auto_now=True)
 
+    def get_topics_in_board(self):
+        topics = Topic.objects.filter(board=self)
+        return topics
+
 class Topic(models.Model):
-    board = models.ForeignKey(to=Board, on_delete=models.CASCADE)
+    board = models.ForeignKey(to=Board, related_name='topics', on_delete=models.CASCADE)
     topic_title = models.CharField(max_length=64, null=False, blank=False)
     topic_text = models.TextField(max_length=512, null=False, blank=False)
     topic_created = models.DateTimeField(verbose_name='date and time the Topic was created', auto_now_add=True)
     topic_modified = models.DateTimeField(verbose_name='date and time the Topic was most recently modified', auto_now=True)
 
 class Comment(models.Model):
-    topic = models.ForeignKey(to=Topic, on_delete=models.CASCADE)
+    topic = models.ForeignKey(to=Topic, related_name='comments', on_delete=models.CASCADE)
     comment_text = models.TextField(max_length=512, null=False, blank=False)
     comment_created = models.DateTimeField(verbose_name='date and time the Comment was created', auto_now_add=True)
     comment_modified = models.DateTimeField(verbose_name='date and time the Comment was most recently modified', auto_now=True)
