168487cc2fa4f0959c7948d03d1f0793210d4ec6
Zachary McGuire <zachary_mcguire@outlook.com>
2020-01-21 07:57:04 +1100

    added index view to display boards and topics.

--- forum/views.py
+++ forum/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponseRedirect
+from django.views import generic
 
-# Create your views here.
+from .models import Board, Topic, Comment
+
+class IndexView(generic.ListView):
+    template_name = 'forum/index.html'
+
+    def get_queryset(self):
+        return Board.objects.all()
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["topic_list"] = Topic.objects.all()
+        return context
+    
+    
