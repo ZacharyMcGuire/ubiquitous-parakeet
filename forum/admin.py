daeab6251d8d35b09f707b380f95c46411d4c1f0
Zachary McGuire <zachary_mcguire@outlook.com>
2020-01-21 07:52:53 +1100

    registered models with admin

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Board, Topic, Comment
+
+admin.site.register(Board)
+admin.site.register(Topic)
+admin.site.register(Comment)
